336f84509be6b2bfb057ca0d278b43619400ea1b
Yanny <larryanrenell@gmail.com>
2022-03-31 21:04:27 -0500

    update assignment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-let sober = true
-if ( sober ) {
-    console.log ( " you can drive ")
+const hasLicense = true;
+const myAge = 25;
+const drivingAge = 18;
+
+if (hasLicense && myAge>=drivingAge) {
+    console.log("you can drive.");
 } else {
-console.log ( " you can not drive ")
+    console.log("you can not drive");
 }
